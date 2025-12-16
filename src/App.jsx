e43dd365522ca18114e8926a990b86d08c960fbb
stefanversterker <stefanversterker@gmail.com>
2025-12-16 16:30:18 +0100

    start opdracht

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="page-container">
             <img src={logo} alt="Company logo"/>
-            <h1>Begin hier met het maken van jouw blog-applicatie!</h1>
+            <h1>Begin hier met het maken van jouw blog-applicatie!!</h1>
         </div>
     )
 }
